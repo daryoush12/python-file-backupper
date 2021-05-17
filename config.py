4fb42533ca33cccc133321da74710c5964cc8386
Daryoush Farsimadan <farsimadan.daryoush@gmail.com>
2021-05-17 23:54:10 +0300

    Fix couple bugs with filepaths and locations

--- config.py
+++ config.py
@@ -1,6 +1,12 @@
 import io
 import json
 
+default_config = {
+  "input": None,
+  "output": None,
+  "backup_cycle": 60
+}
+
 class BackupConfig():
     def __init__(self):
         try:
@@ -8,9 +14,24 @@ class BackupConfig():
                 config = json.load(file)
                 self.cycle = config['backup_cycle']
                 self.input = config['input']
-                self.output = config['output']
+                self.output = config['output']                   
         except:
-                print("Could not init configuration. config.json file not found")
+                print("Config.json file not found. Creating one with defaults..")
+                in_path = input("Enter path into backuppable folder:")
+                out_path = input("Enter output path:")
+                cycle = input("How often should we backup the files? (in seconds):")
+
+                default_config = {"input":in_path, "output":out_path, "backup_cycle":int(cycle)}
+                
+                with open("config.json", "w") as write_file:
+                    json.dump(default_config, write_file)
+
+                self.cycle = default_config['backup_cycle']
+                self.input = default_config['input']
+                self.output = default_config['output']
+
+
+
 
     def getCycle(self):
         return self.cycle
--- main.py
+++ main.py
@@ -5,6 +5,7 @@ from distutils.dir_util import copy_tree
 from datetime import date
 import os
 from time import time
+from distutils.errors import DistutilsFileError
 
 
 path = os.getcwd()
@@ -13,11 +14,15 @@ today = date.today()
 
 #TODO: Test for missing config options and bad filepaths.
 async def main():
-    print("Backupper has started. Running cycle.")
-    while True:   
-        await asyncio.sleep(settings.cycle)
-        newPath = f"{settings.output}/{today.__str__()}_{time()}"
-        copy_tree(settings.input, newPath)
-        print(f"Backupping from {settings.input} to {newPath}")
+    try:
+        print("Backupper has started. Running cycle.")
+        while True:   
+            newPath = f"{settings.output}/{today.__str__()}_{time()}"
+            copy_tree(settings.input, newPath)
+            print(f"Backupping from {settings.input} to {newPath}")
+            await asyncio.sleep(settings.cycle)
+    except DistutilsFileError:
+        print("Provided folder or file not found. Check your configurations.")
+        
     
 asyncio.run(main())
